7606076ec6eaedb4a193ec7fe9ea1968bae18cad
Rafaela-Quinzel <71155947+Rafaela-Quinzel@users.noreply.github.com>
2021-04-03 01:34:52 -0300

    Adicionando alterações na estilização

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -7,33 +7,115 @@ function Projects() {
     return (
         <S.MainContainer className="my-projects">
             <S.TitlePage>Meus Projetos</S.TitlePage>
+
+
             <S.CardProjectOne
-                ata-aos="fade-zoom-in" data-aos-duration="1500"
-                data-aos="fade-right"
-                data-aos-offset="300"
-                data-aos-easing="ease-in-sine"
+            ata-aos="fade-zoom-in" data-aos-duration="1500"
+            data-aos="fade-right"
+            data-aos-offset="300"
+            data-aos-easing="ease-in-sine"
             >
-                <S.TitleCard>Projeto 01</S.TitleCard>
-                <S.Text>Ver projeto</S.Text>
+                <S.ImageContainer>
+                    <S.Text>
+                        <S.TitleProject>Labe X</S.TitleProject>
+                        <br />
+                        <S.AboutProject margin="20px">
+                            Trata-se de uma aplicação com funcionalidades básicas de um site para viagens espaciais.
+                        </S.AboutProject>
+                        <br />
+                        <S.SeeProject
+                            href="https://github.com/Rafaela-Quinzel/LabeX"
+                            target="_blank"
+                        >
+                            VER PROJETO
+                        </S.SeeProject>
+                    </S.Text>
+                </S.ImageContainer>
+                <S.TextContainer>
+                    <S.TitleCard>Labe X</S.TitleCard>
+                    <S.DescriptionProject>
+                        Trata-se de uma aplicação com funcionalidades básicas de um site para viagens espaciais.
+                        <br />
+                        Esse projeto Front-end foi desenvolvido com Create React App.
+                    </S.DescriptionProject>
+                    <S.ButtonCard
+                        href="https://github.com/Rafaela-Quinzel/LabeX"
+                        target="_blank"
+                    >
+                        VER PROJETO
+                    </S.ButtonCard>
+                </S.TextContainer>
             </S.CardProjectOne>
+
             <S.CardProjectTwo
                 ata-aos="fade-zoom-in" data-aos-duration="1500"
                 data-aos="fade-left"
                 data-aos-offset="300"
                 data-aos-easing="ease-in-sine"
             >
-                <S.TitleCard>Projeto 02</S.TitleCard>
-                <S.Text>Ver projeto</S.Text>
+                <S.ImageContainer>
+                    <S.Text>
+                        <S.TitleProject>LabEddit</S.TitleProject>
+                        <br />
+                        <S.AboutProject margin="20px">
+                            Projeto Front-end baseado na rede social Reddit.
+                            Esse projeto foi desenvolvido com Create React App.
+                        </S.AboutProject>
+                        <br />
+                        <S.SeeProject
+                            href="https://github.com/Rafaela-Quinzel/LabEddit"
+                            target="_blank"
+                        >
+                            VER PROJETO
+                        </S.SeeProject>
+                    </S.Text>
+                </S.ImageContainer>
+
+                <S.TextContainer>
+                    <S.TitleCard>LabEddit</S.TitleCard>
+                    <S.DescriptionProject>
+                        Projeto Front-end baseado na rede social Reddit.
+                        Esse projeto foi desenvolvido com Create React App.
+                    </S.DescriptionProject>
+                    <S.ButtonCard
+                        href="https://github.com/Rafaela-Quinzel/LabEddit"
+                        target="_blank"
+                    >
+                        VER PROJETO
+                    </S.ButtonCard>
+                </S.TextContainer>
             </S.CardProjectTwo>
+
             <S.CardProjectThree
                 ata-aos="fade-zoom-in" data-aos-duration="1500"
                 data-aos="fade-right"
                 data-aos-offset="300"
                 data-aos-easing="ease-in-sine"
             >
-                <S.TitleCard>Projeto 03</S.TitleCard>
-                <S.Text>Ver projeto</S.Text>
+                <S.ImageContainer>
+                    <S.Text>
+                        <S.TitleProject>Projeto SoundLab</S.TitleProject>
+                        <br />
+                        <S.AboutProject margin="20px">
+                            Projeto Front-end que trata-se de uma aplicação com funcionalidades básicas de um site para streaming de músicas.
+                        </S.AboutProject>
+                        <br />
+                        <S.SeeProject href="https://github.com/Rafaela-Quinzel/Frontend-Project" target="_blank">VER PROJETO</S.SeeProject></S.Text>
+                </S.ImageContainer>
+                <S.TextContainer>
+                    <S.TitleCard>SoundLab</S.TitleCard>
+                    <S.DescriptionProject>
+                        Projeto Front-end que trata-se de uma aplicação com funcionalidades básicas de um site para streaming de músicas.
+                        Esse projeto foi desenvolvido com Create React App.
+                    </S.DescriptionProject>
+                    <S.ButtonCard
+                        href="https://github.com/Rafaela-Quinzel/Frontend-Project" target="_blank"
+                    >
+                        VER PROJETO
+                    </S.ButtonCard>
+                </S.TextContainer>
             </S.CardProjectThree>
+
             <S.ButtonProjects title="Ver mais Projetos">
                 <S.TextButton
                     href="https://github.com/Rafaela-Quinzel?tab=repositories"
--- src/components/Projects/styled.js
+++ src/components/Projects/styled.js
@@ -3,16 +3,17 @@ import styled from "styled-components"
 
 export const MainContainer = styled.div`
     width: 100%;
-    height: 797px;
+    height: 1250px;
     background-color: #f2f2f2;
+
     @media(max-width: 800px) {
-       width: 362.1%;
+       width: 78rem;
+       height: 1200px;
     }
 `
 
 export const TitlePage = styled.h1`
-    margin: 2% auto 4% auto;
-    font-size: 25px;
+    margin: 4% auto 5% auto;
     font-weight: bold;
     font-stretch: normal;
     font-style: normal;
@@ -20,61 +21,233 @@ export const TitlePage = styled.h1`
     letter-spacing: -0.15px;
     text-align: center;
     color: #8D1C31;
+
+    @media(max-width: 800px) {
+        margin: 5% auto 10% auto;
+        font-size: 30px;
+    }
 `
 
 export const CardProjectOne = styled.div`
-    border: 1px solid red;
+    box-shadow: inset 0 0 1em #1a1c1daf, 0 0 1em #272a2baf;
+    background-image: url("/labexhome.png");
+    background-size: 95% 90%;
+    background-repeat: no-repeat;
+    background-position: center;
     border-radius: 10px;
-    width: 40vw;
-    height: 30vh;
-    margin: 4% 2% 0 5%;
+    width: 35vw;
+    height: 44vh;
+    margin: 4% 2% 0 8%;
     cursor: pointer;
-    &:hover{
-        background-color: gray;
+
+    @media(max-width: 800px) {
+        width: 390px;
+        height: 42vh;
+        padding: 0;
     }
 `
 
 export const CardProjectTwo = styled.div`
-    border: 1px solid red;
+    box-shadow: inset 0 0 1em #1a1c1daf, 0 0 1em #272a2baf;
+    background-image: url("/labeddithome.png");
+    background-size: 95% 90%;
+    background-repeat: no-repeat;
+    background-position: center;
     border-radius: 10px;
-    width: 40vw;
-    height: 30vh;
-    margin: 0 5% 0 4%;
+    width: 35vw;
+    height: 44vh;
+    margin: 0 8% 0 8%;
     float: right;
     cursor: pointer;
-    &:hover{
-        background-color: gray;
+
+    @media(max-width: 800px) {
+        width: 390px;
+        height: 42vh;
+        padding: 0; 
     }
 `
 
 export const CardProjectThree = styled.div`
-    border: 1px solid red;
+    background-image: url("/soundlabhome.png");
+    background-size: 95% 90%;
+    background-repeat: no-repeat;
+    background-position: center;
+    box-shadow: inset 0 0 1em #1a1c1daf, 0 0 1em #272a2baf;
     border-radius: 10px;
-    width: 40vw;
-    height: 30vh;
-    margin: 15% 2% 4% 5%;
+    width: 35vw;
+    height: 44vh;
+    margin: 22% 2% 4% 8%;
     cursor: pointer;
-    &:hover{
-        background-color: gray;
+
+    @media(max-width: 800px) {
+        width: 390px;
+        height: 42vh;
+        padding: 0; 
+        margin-top: 24%;
     }
 `
 
-export const TitleCard = styled.h2`
-  width: 172px;
-  height: 52px;
-  margin: 0 200px 0 0;
-  font-weight: bold;
-  font-stretch: normal;
-  font-style: normal;
-  line-height: normal;
-  letter-spacing: -0.25px;
-  color: #8d1c31;
+export const ImageContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    cursor: pointer;
+    width: 100%;
+    height: 100%;
+    border-radius: 10px;
+    color: transparent;
+ 
+    @media(max-width: 800px) {
+        display: none;
+    }
 `
+
+export const Text = styled.div`
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: bold;
+    line-height: normal;
+    letter-spacing: -0.28px;
+    text-align: center;
+    color: transparent;
+    text-decoration: none;
+  
+    &:hover{
+        background-color: rgba(0, 0, 0, 0.5);
+        color: #ffffff;
+        padding: 10% 5% 0 5%;
+        border-radius: 10px;
+        border: 2px solid white;  
+    }
+
+    @media(max-width: 800px) {
+        display: none;
+    } 
+`
+
+export const TitleProject = styled.h1`
+    &:hover{
+    color: ${props => props.color};
+    }
+`
+
+export const AboutProject = styled.p`
+    font-size: 22px;
+    margin: ${props => props.margin};
+`
+
+export const SeeProject = styled.a`
+    font-size: 30px;
+    text-decoration: none;
+
+    &:hover{
+       background-color: #8D1C31;
+       border-radius: 10px;
+       padding: 2% 5% 2% 5%;
+       font-size: 34px;
+       text-decoration: none;
+    }
+
+    &:active{
+        text-decoration: none;
+        color: white;
+    }
+
+    &:visited{
+        text-decoration: none;
+        color: white;
+    }
+`
+
+
+export const TextContainer = styled.div`
+    display: none;
+
+    @media(max-width: 800px) {
+        display: grid;
+        width: 100%;
+        height: 100%;
+        padding: 4%;
+        border-radius: 10px;
+        font-weight: normal;
+        font-stretch: normal;
+        font-style: normal;
+        line-height: normal;
+        letter-spacing: -0.28px;
+        text-align: center;
+        color: white;
+        background-color: rgba(0, 0, 0, 0.6);
+    }
+`
+
+export const TitleCard = styled.h1`
+    display: none;
+
+    @media(max-width: 800px){
+        font-size: 3rem;
+        display: block;
+        font-weight: bold;
+        font-stretch: normal;
+        font-style: normal;
+        line-height: normal;
+        letter-spacing: -0.20px;
+        color: #33A6A6;
+        text-shadow: 1px 1px 1px #8D1C31;
+        cursor: default;
+    }
+`
+
+export const DescriptionProject = styled.p`
+    display: none;
+
+    @media(max-width: 800px) {
+        display: flex;
+        font-size: 16px;
+        font-weight: normal;
+        font-stretch: normal;
+        font-style: normal;
+        line-height: 1.6rem;
+        letter-spacing: -0.20px;
+        text-align: center;
+        cursor: default;
+    }
+` 
+
+export const ButtonCard = styled.a`
+    display: none;
+
+    @media(max-width: 800px) {
+        display: block;
+        border-radius: 8px;
+        padding: 4%;
+        margin: 5% auto 4% auto;
+        width: 160px;
+        height: 45px;
+        font-size: 16px;
+        font-weight: normal;
+        font-stretch: normal;
+        font-style: normal;
+        line-height: normal;
+        letter-spacing: -0.28px;
+        text-align: center;
+        background-color:  #8D1C31;
+        text-decoration: none;
+    }
+
+    &:active{
+        text-decoration: none;
+        color: white;
+    }
+
+    &:visited{
+        text-decoration: none;
+        color: white;
+    }
+` 
 
 export const ButtonProjects = styled.button`
     width: 250px;
     height: 40px;
-    margin: 5px auto 0 42%;
+    margin: 45px auto 0 42%;
     padding: 5px 30px 7px 31px;
     border-radius: 5px;
     box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.5);
@@ -87,8 +260,11 @@ export const ButtonProjects = styled.button`
         background-color: #052a3a; 
         transition: background-color 1s;
     }
+
     @media(max-width: 800px) {
-        margin: 70px 12px 0 0;
+        margin: 10% auto 0 40%;
+        width: 280px;
+        height: 60px;
     }
 `
 
@@ -102,16 +278,10 @@ export const TextButton = styled.a`
     text-align: center;
     color: #ffffff;
     text-decoration: none;
+
+    @media(max-width: 800px) {
+        font-size: 18px;
+    }
 `
 
-export const Text = styled.a`
-    font-size: 16px;
-    font-weight: normal;
-    font-stretch: normal;
-    font-style: normal;
-    line-height: normal;
-    letter-spacing: -0.28px;
-    text-align: center;
-    color: #ffffff;
-    text-decoration: none;
-` 
+
